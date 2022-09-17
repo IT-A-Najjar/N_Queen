d5687412d8352f3c949c8c269259f2e4c91d8750
Abdullah Najjar <eng.abdullah.najjar.2001@gmail.com>
2022-09-17 10:34:54 +0300

    end

---
+++ N_QuanHau/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N_QuanHau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("enter the number n>3: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Queens q = new Queens(n);
+                if (q.Solve(0))
+                    q.Display();
+                else
+                    Console.WriteLine("the array is empte");
+
+                Console.WriteLine("enter agen:");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ N_QuanHau/Queens.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace N_QuanHau
+{
+    class Queens
+    {
+        public int N;
+        public int[,] Board;
+
+        public Queens(int n)
+        {
+            N = n;
+            Board = new int[N, N];
+            
+        }
+        public bool isSafe(int r,int c)
+        {
+            int i, j;
+            for (i = 0; i < c; i++)
+                if (Board[r, i] == 1)
+                    return false;
+            for (i = r, j = c; i >= 0 && j >= 0; i--, j--)
+                if (Board[i, j] == 1)
+                    return false;
+            for (i = r, j = c; i < N && j >= 0; i++, j--)
+                if (Board[i, j] == 1)
+                    return false;
+
+            return true;
+        }
+
+        public bool Solve(int c)
+        {
+            if (c>= N)
+                return true;
+            for(int i=0;i < N; i++)
+            {
+                if (isSafe(i, c))
+                {
+                    Board[i, c] = 1;
+                    if (Solve(c +1))
+                        return true;
+                    Board[i, c] = 0;
+                }
+            }
+            return false;
+        }
+        public void Display()
+        {
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < N; j++)
+                {
+                    if (Board[i, j] == 1)
+                        Console.Write("Q");
+                    else
+                        Console.Write("*");
+                    Console.Write("  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
